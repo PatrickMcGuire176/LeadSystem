2ac31c2e11c203dc3ed655cccebea59cf77f387b
patmcg <patrickmcguire176@gmail.com>
2022-08-23 16:01:32 -0600

    Initial commit

---
+++ src/test/java/com/mcguire/leadsystem/LeadsystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mcguire.leadsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeadsystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
